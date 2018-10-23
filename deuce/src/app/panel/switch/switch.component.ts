b3e0f184c9eaa812b8c7d4ce669ddf556b5d85a8
gerardward3 <gerardward3@gmail.com>
2018-10-23 02:04:13 +0100

    Adds attempt at making switches interactive.

--- deuce/src/app/panel/switch/switch.component.ts
+++ deuce/src/app/panel/switch/switch.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./switch.component.css']
 })
 export class SwitchComponent implements OnInit {
+  off = 'src/assets/img/switch-off.png';
+  on = 'src/assets/img/switch-on.png';
 
+  change(event: any) {
+    const imgElement = <HTMLImageElement>document.getElementById('switch');
+    console.log(imgElement.src);
+
+    if (imgElement.src === this.off) {
+      imgElement.src = this.on;
+      console.log('ON');
+    } else {
+      imgElement.src = this.on;
+      console.log('OFF');
+    }
+
+  }
   constructor() { }
 
   ngOnInit() {
